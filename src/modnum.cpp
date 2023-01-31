99a1fd62d3023039778f6c10b53969d4150f9a0d
Quoc Hung <truongquochung312@gmail.com>
2023-02-01 05:56:44 +0700

    ...

--- src/modnum.cpp
+++ src/modnum.cpp
@@ -9,44 +9,15 @@ struct modnum {
     int value() const { return x; }
     modnum inv() const { return pow(MOD-2); }
 
-    modnum& operator+=(const modnum& o) {
-        x += o.x;
-        if (x >= MOD) x -= MOD;
-        return *this;
-    }
-    modnum& operator-=(const modnum& o) {
-        x -= o.x;
-        if (x < 0) x += MOD;
-        return *this;
-    }
-    modnum& operator*=(const modnum& o) {
-        x = int(int64_t(x) * int64_t(o.x) % MOD);
-        return *this;
-    }
-    modnum& operator/=(const modnum& o) {
-        return *this *= o.inv();
-    }
+    modnum& operator+=(const modnum& o) { x += o.x; if (x >= MOD) x -= MOD; return *this; }
+    modnum& operator-=(const modnum& o) { x -= o.x; if (x < 0) x += MOD; return *this; }
+    modnum& operator*=(const modnum& o) { x = int(int64_t(x) * int64_t(o.x) % MOD); return *this; }
+    modnum& operator/=(const modnum& o) { return *this *= o.inv(); }
 
-    friend modnum operator+(const modnum& a, const modnum& b) {
-        modnum ans = a;
-        ans += b;
-        return ans;
-    }
-    friend modnum operator-(const modnum& a, const modnum& b) {
-        modnum ans = a;
-        ans -= b;
-        return ans;
-    }
-    friend modnum operator*(const modnum& a, const modnum& b) {
-        modnum ans = a;
-        ans *= b;
-        return ans;
-    }
-    friend modnum operator/(const modnum& a, const modnum& b) {
-        modnum ans = a;
-        ans /= b;
-        return ans;
-    }
+    friend modnum operator+(const modnum& a, const modnum& b) { modnum ans = a; ans += b; return ans; }
+    friend modnum operator-(const modnum& a, const modnum& b) { modnum ans = a; ans -= b; return ans; }
+    friend modnum operator*(const modnum& a, const modnum& b) { modnum ans = a; ans *= b; return ans; }
+    friend modnum operator/(const modnum& a, const modnum& b) { modnum ans = a; ans /= b; return ans; }
 
     modnum pow(int64_t b) const {
         modnum ans(1);
